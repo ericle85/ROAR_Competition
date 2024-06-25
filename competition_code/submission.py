ebc88f28f0718b2baa07f56723ddf769cf4c84cd
ericle85 <leeric5040@gmail.com>
2024-06-25 16:07:00 +0700

    just some minor edits (220s)

--- competition_code/submission.py
+++ competition_code/submission.py
@@ -51,13 +51,17 @@ class RoarCompetitionSolution:
         vehicle_rotation = self.rpy_sensor.get_last_gym_observation()
         vehicle_velocity = self.velocity_sensor.get_last_gym_observation()
 
-        self.current_waypoint_idx = 10
+
+
+        self.current_waypoint_idx = 13
         self.current_waypoint_idx = filter_waypoints(
             vehicle_location,
             self.current_waypoint_idx,
             self.maneuverable_waypoints
         )
 
+
+   
 
     async def step(
         self
@@ -74,10 +78,7 @@ class RoarCompetitionSolution:
         vehicle_rotation = self.rpy_sensor.get_last_gym_observation()
         vehicle_velocity = self.velocity_sensor.get_last_gym_observation()
         vehicle_velocity_norm = np.linalg.norm(vehicle_velocity)
-
-        
-
-        
+       
         
         # Find the waypoint closest to the vehicle
         self.current_waypoint_idx = filter_waypoints(
@@ -86,91 +87,68 @@ class RoarCompetitionSolution:
             self.maneuverable_waypoints
         )
 
-        def getLookAheadDistance(speed):
+        def getLookAheadDistance():
             currentSpeed =np.linalg.norm(self.velocity_sensor.get_last_gym_observation()) * 3.6
-            # if (currentSpeed >= 110):
-            #     num = 13
-            # if (currentSpeed >= 160):
-            #     num = 16
-            # if (currentSpeed >= 180):
-            #     num = 20
-            if currentSpeed >= 195:
-                num = 50
+            if currentSpeed >= 160:
+                num = 35
             else:
                 num = 11
-
             return num
+        waypointClose = self.maneuverable_waypoints[(self.current_waypoint_idx +  getLookAheadDistance()) % len(self.maneuverable_waypoints)]
+
+        waypointMedium = self.maneuverable_waypoints[(self.current_waypoint_idx + int(getLookAheadDistance() + 15) ) % len(self.maneuverable_waypoints)]
+
+        waypointFar = self.maneuverable_waypoints[(self.current_waypoint_idx + int( getLookAheadDistance() + 30) ) % len(self.maneuverable_waypoints)]
+
+        waypoint_to_follow = waypointClose
+        #x and y components of each waypoint
+        point1 = (waypointMedium.location[0], waypointMedium.location[1])
+        point2 = (waypointClose.location[0], waypointClose.location[1])
+        point3 = (waypointFar.location[0], waypointFar.location[1])
+      
+        l1 = round(math.dist(point1, point2), 5)
+        l2 = round(math.dist(point2, point3), 5)
+        l3 = round(math.dist(point1, point3),5)
+        zeroChecker = 0.01
+        sp = (l1 + l2 + l3) / 2
+        area_squared =  sp * (sp - l1) * (sp - l2) * (sp - l3)
+        targetSpeed = 20
+        # Check if any side lengths are very small
+        if l1 < zeroChecker or l2 < zeroChecker or l3 < zeroChecker:
+            targetSpeed = 350  # Set a high value as a default or handle differently
+        else:
+            # Check if area_squared is non-zero before calculating radius
             
+            if area_squared > 0:
 
-        # We use the 3rd waypoint ahead of the current waypoint as the target waypoint
-
-        waypoint_to_follow = self.maneuverable_waypoints[(self.current_waypoint_idx + 11) % len(self.maneuverable_waypoints)]
-        
-        def getCurve(current_waypoint_idx):
-            waypointClose = self.maneuverable_waypoints[(current_waypoint_idx + getLookAheadDistance(vehicle_velocity_norm)) % len(self.maneuverable_waypoints)]
-
-            waypointMedium = self.maneuverable_waypoints[(current_waypoint_idx + getLookAheadDistance(vehicle_velocity_norm) + 15)  % len(self.maneuverable_waypoints)]
-
-            waypointFar = self.maneuverable_waypoints[(current_waypoint_idx +  getLookAheadDistance(vehicle_velocity_norm) + 30) % len(self.maneuverable_waypoints)]
-           
-            point1 = (waypointMedium.location[0], waypointMedium.location[1])
-            point2 = (waypointClose.location[0], waypointClose.location[1])
-            point3 = (waypointFar.location[0], waypointFar.location[1])
- 
-            l1 = round(math.dist(point1, point2), 3)
-            l2 = round(math.dist(point2, point3), 3)
-            l3 = round(math.dist(point1, point3),3)
-            zeroChecker = 0.01
-            sp = (l1 + l2 + l3) / 2
-            area_squared =  sp * (sp - l1) * (sp - l2) * (sp - l3)
-            targetSpeed = 1000
-            # Check if any side lengths are very small
-            if (l1 < zeroChecker) or (l2 < zeroChecker) or (l3 < zeroChecker):
-                targetSpeed = 1000  # Set a high value as a default or handle differently
+                def default_value():
+                         return 1.8
+                frictionCoefficents = defaultdict(default_value,  {
+                            0 : 1.6,
+                            #4 : .8,
+                            6 : 1.1,
+                            7 : 2.2,
+                            9 : 1.7
+                } )
+                                
+                coFriction = 1.8  
+                if 0 <self.current_waypoint_idx <= 2775:
+                    coFriction = frictionCoefficents[int(self.current_waypoint_idx / 277.5)]
+                if (2775 <self.current_waypoint_idx <= 2775 * 2):
+                    coFriction = frictionCoefficents[int((self.current_waypoint_idx - 2775 )/ 277.5)]
+                if (2775 * 2 <self.current_waypoint_idx <= 2775 * 3):
+                    coFriction = frictionCoefficents[int((self.current_waypoint_idx - 2775 * 2 )/ 277.5)]
                 
+                radius = (l1 * l2 * l3) / (4 * math.sqrt(area_squared))
+                globalRadius = radius
+                targetSpeed = math.sqrt(9.81 * coFriction * radius)
+               # print("radius: " + str(radius))
             else:
-                # Check if area_squared is non-zero before calculating radius
-                
-                if area_squared > 0:
-                    
-                    radius = (l1 * l2 * l3) / (4 * math.sqrt(area_squared))
-                    globalRadius = radius
-
-                    
-                    def default_value():
-                        return 1.8
-                    frictionCoefficents = defaultdict(default_value,  {
-                        0 : 1.6,
-                        4 : .8,
-                        6 : 1.1,
-                        7 : 2.2,
-                        9 : 1.7
-                    } )
-                            
-                    
-                    if 0 <current_waypoint_idx <= 2775:
-                        currentSectionIdx = frictionCoefficents[int(current_waypoint_idx / 277.5)]
-                    if (2775 <current_waypoint_idx <= 2775 * 2):
-                        currentSectionIdx = frictionCoefficents[int((current_waypoint_idx - 2775 )/ 277.5)]
-                    if (2775 * 2 <current_waypoint_idx <= 2775 * 3):
-                        currentSectionIdx = frictionCoefficents[int((current_waypoint_idx - 2775 * 2 )/ 277.5)]
-                    
-
-                    coFriction = frictionCoefficents[currentSectionIdx]
+                # Handle case where area_squared is zero
+                # This could involve setting targetSpeed to a default value or handling it differently
+                targetSpeed = float('inf')  # Example of setting targetSpeed to a high value
 
 
-
-                    targetSpeed = math.sqrt(9.81 * coFriction * radius)
-                    
-            return max(20,targetSpeed)
-
-
-            
-        close = getCurve(self.current_waypoint_idx)
-        medium = getCurve(self.current_waypoint_idx + 30)
-        far = getCurve(self.current_waypoint_idx + 60)
-
-        targetSpeed = close #min(close, medium,far)
         # Calculate delta vector towards the target waypoint
         vector_to_waypoint = (waypoint_to_follow.location - vehicle_location)[:2]
         heading_to_waypoint = np.arctan2(vector_to_waypoint[1],vector_to_waypoint[0])
@@ -180,44 +158,46 @@ class RoarCompetitionSolution:
 
         # Proportional controller to steer the vehicle towards the target waypoint
         steer_control = (
-            -10.0 / np.sqrt(vehicle_velocity_norm) * delta_heading / np.pi
+            -8.0 / np.sqrt(vehicle_velocity_norm) * delta_heading / np.pi
         ) if vehicle_velocity_norm > 1e-2 else -np.sign(delta_heading)
         steer_control = np.clip(steer_control, -1.0, 1.0)
-
-        
-        #throttle_control = 0.8 * (targetSpeed - vehicle_velocity_norm)
        
-    
-
-        maxSpeed = 350
         speed_error = targetSpeed - vehicle_velocity_norm * 3.6  # Convert vehicle speed to kph
-        Kp = 5
+        Kp = 8
 
-        if abs(speed_error) > 5:  # Adjust threshold as needed
-            throttle_control = Kp * speed_error
-        elif abs(speed_error) < 5:
-            throttle_control = .03 * speed_error
+            # Determine if reversing is necessary
+        if targetSpeed < 0.75 * vehicle_velocity_norm * 3.6: 
+            throttle_control = -1  # Reverse with full throttle
+            
         else:
-            throttle_control = 0
-
-        # if speed_error > 100:  # Adjust threshold as needed
-        #     throttle_control = 1
-        # elif speed_error < -100:
-        #     throttle_control = -1  # Small error, maintain current throttle
-        # else: throttle_control = .5
+            # Apply proportional controller for throttle
+            speed_error = targetSpeed - vehicle_velocity_norm * 3.6  # Convert vehicle speed to kph
+            Kp = 5
+            throttle_control = Kp * speed_error
 
         
-        print("target: " + str(int(targetSpeed)) + "| curr kph: " + str(int(vehicle_velocity_norm * 3.6)))
+        # Apply brake if speed error is small
+        if abs(speed_error) < 3:
+            brake_control = .75
+            
+        else:
+            brake_control = 0.0
+            
+
+       # print("target: " + str(round(targetSpeed,3)) + "| curr kph: " + str(int(vehicle_velocity_norm * 3.6)))
+        
+
+        
 
         control = {
             "throttle": np.clip(throttle_control, 0.0, 1.0),
             "steer": steer_control,
             "brake": np.clip(-throttle_control, 0.0, 1.0),
             "hand_brake": 0.0,
-            "reverse": 0,
+            "reverse": 0 if throttle_control < 0 else 0,
             "target_gear": 0
         }
         await self.vehicle.apply_action(control)
         return control
-    
+
     
